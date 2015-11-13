15f7daa17bdb556775902c39a02eaebebe32b6cc
Agostino Cavarero <agostino.cavarero@ig.com>
2015-11-13 12:54:50 +0000

    use cin instead of cl arguments and cout instead of printf

--- divisibility.cpp
+++ divisibility.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 int main(int argc, char *argv[]) {
-    unsigned long long k = std::stoull(argv[1]);
-    long long a = std::stoll(argv[2]);
-    long long b = std::stoll(argv[3]);
-    std::printf("%I64d", (b - a) / k + 1);
+    unsigned long long k;
+    long long a;
+    long long b;
+    cin >> k >> a >> b;
+    cout << (b - a) / k + 1;
     return 0;
 }
