c9ce4cc6dcdc94f260d720d151617d7b430435e0
ago <omarebo@gmail.com>
2015-11-15 17:44:18 +0000

    correction: modulo doesn't work with negative divisor

--- divisibility.cpp
+++ divisibility.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 int main(int argc, char *argv[]) {
-    unsigned long long k;
+    long long k;
     long long a;
     long long b;
     cin >> k >> a >> b;
-    long long res = (b - a) / k + 1;
+    long long res = (b - a + 1) / k;
     long long mod = (b-a+1)%k;
-    mod += mod % k ? 0 : k;
-    if ((a%k+k)%k >= mod)
-        cout << res - 1;
+    long long mod2 = (a % k + k) % k;
+    if (!mod2) mod2 = k;
+    if (mod2 > k-mod)
+        cout << res + 1;
     else
         cout << res;
 }
