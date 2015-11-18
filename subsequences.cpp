477e1132da3b61935decb856e4ff67ac95a7c9bc
ago <omarebo@gmail.com>
2015-11-18 18:44:13 +0000

    fix subsequences

--- subsequences.cpp
+++ subsequences.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
 int n, k, l;
-int main (int args, *char[] argv) {
-    cout << n << k;
+int main (int args, char *argv[]) {
+    cin >> n >> k;
     int v[n];
     for(int i = 0; i < n; ++i) cin >> v[i];
-    int m[k+1][n];
+    long long m[k+1][n];
     for(int i = 0; i <= k; ++i) m[i][0] = 0;
     for(int i = 0; i < n; ++i) m[0][i] = 1;
-    for(int i = 0; i < k; ++i)
-        for(int j = 1; i < n; ++i) {
-            m[i][j] = m[k][j-1];
-            l = j - 1;
-            while(l > -1 && v[l] > v[j]) --l;
-            if (l != -1) m[i][j] += m[i - 1][l]
+    for(int i = 1; i <= k; ++i)
+        for(int j = 1; j < n; ++j) {
+            m[i][j] = 0LL;
+            for(int l = 0; l < j; ++l)
+                if(v[l] < v[j])
+                    m[i][j] += m[i-1][l];
         }
+    cout << accumulate(m[k], m[k]+n, 0LL);
 }
